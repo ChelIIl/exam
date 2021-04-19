25f8a5b21746793472200564dd00a5ef1850b899
stu-pksp117 <stu-pksp117@AMDVLA3511GW05>
2021-04-19 12:30:06 +0300

    Products

---
+++ src/Products.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    class Products
+    {
+        string fishtype = "";
+        string proiz = "";
+        double price;
+
+        public string SetFishType
+        {
+            get { return fishtype; }
+            set { fishtype = value; }
+        }
+
+        public string SetProiz
+        {
+            get { return proiz; }
+            set { proiz = value; }
+        }
+
+        public double SetPrice
+        {
+            get { return price; }
+            set { price = value; }
+        }
+    }
+}
